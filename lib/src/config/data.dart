bd8c64ea28a37d34c9a61bd3e63900637f6d83a9
Lucas França <lucas.fnev@gmail.com>
2023-05-26 10:34:49 -0300

    Creating product tile

--- lib/src/config/data.dart
+++ lib/src/config/data.dart
@@ -1,61 +1,61 @@
-import 'package:greengrocer/src/models/item.dart';
+import 'package:greengrocer/src/models/product.dart';
 
-Item apple = Item(
+Product apple = Product(
   description:
-      'A melhor maçã da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+      'A melhor maçã da região e que conta com o melhor preço de qualquer quitanda. Este produto conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
   image: 'assets/fruits/apple.png',
   name: 'Maçã',
   price: 5.5,
   unit: 'kg',
 );
 
-Item grape = Item(
+Product grape = Product(
   image: 'assets/fruits/grape.png',
   name: 'Uva',
   price: 7.4,
   unit: 'kg',
   description:
-      'A melhor uva da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+      'A melhor uva da região e que conta com o melhor preço de qualquer quitanda. Este produto conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
 );
 
-Item guava = Item(
+Product guava = Product(
   image: 'assets/fruits/guava.png',
   name: 'Goiaba',
   price: 11.5,
   unit: 'kg',
   description:
-      'A melhor goiaba da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+      'A melhor goiaba da região e que conta com o melhor preço de qualquer quitanda. Este produto conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
 );
 
-Item kiwi = Item(
+Product kiwi = Product(
   image: 'assets/fruits/kiwi.png',
   name: 'Kiwi',
   price: 2.5,
   unit: 'un',
   description:
-      'O melhor kiwi da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+      'O melhor kiwi da região e que conta com o melhor preço de qualquer quitanda. Este produto conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
 );
 
-Item mango = Item(
+Product mango = Product(
   image: 'assets/fruits/mango.png',
   name: 'Manga',
   price: 2.5,
   unit: 'un',
   description:
-      'A melhor manga da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+      'A melhor manga da região e que conta com o melhor preço de qualquer quitanda. Este produto conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
 );
 
-Item papaya = Item(
+Product papaya = Product(
   image: 'assets/fruits/papaya.png',
   name: 'Mamão papaya',
   price: 8,
   unit: 'kg',
   description:
-      'O melhor mamão da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+      'O melhor mamão da região e que conta com o melhor preço de qualquer quitanda. Este produto conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
 );
 
 // Lista de produtos
-List<Item> items = [
+List<Product> products = [
   apple,
   grape,
   guava,
--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,11 +1,11 @@
-class Item {
+class Product {
   String name;
   String image;
   String unit;
   double price;
   String description;
 
-  Item(
+  Product(
       {required this.name,
       required this.image,
       required this.unit,
